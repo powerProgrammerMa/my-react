78c9d372b6a548d5e6700ac74730f6cbac878bb4
majiansong <mjs1706@sina.com>
2021-05-30 17:46:15 +0800

    pp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: mikey.zhaopeng 
  * @Date: 2021-05-30 17:26:08 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2021-05-30 17:39:57
+ * @Last Modified time: 2021-05-30 17:46:06
  */
 
 
@@ -37,7 +37,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                I am react_app........
+                I am react_app..............
             </div>
         )
     }
